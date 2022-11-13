66101f35b36b9b45f9ef7e5947edc545e415a7a8
Roller Lo <jasonbh02@gmail.com>
2022-11-13 19:18:43 +0800

    chore: eslint & prettier packages & config files

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+		jest: true,
+	},
+	extends: ['eslint:recommended', 'plugin:react/recommended'],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		'react/prop-types': 'off',
+		'react/react-in-jsx-scope': 'off',
+	},
+}
