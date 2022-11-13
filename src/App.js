3e1b191e96b76800fd6157be0e9ed4cf6391868d
Roller Lo <jasonbh02@gmail.com>
2022-11-13 19:57:16 +0800

    feat: add demo example for router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import logo from './logo.svg'
 import './App.css'
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
+import Home from './Home.js'
+import About from './About.js'
+import Users from './Users.js'
 
 function App() {
 	return (
 		<div className="App">
+			<Router>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/about">About</Link>
+						</li>
+						<li>
+							<Link to="/users">Users</Link>
+						</li>
+					</ul>
+				</nav>
+				<Routes>
+					<Route path="/about" element={<About />} />
+					<Route path="/users" element={<Users />} />
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</Router>
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
---
+++ src/Users.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Users = () => <div>users</div>
+
+export default Users
