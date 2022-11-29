1fe44df469f81c8e55ce309b55818ac670bd206b
Roller Lo <jasonbh02@gmail.com>
2022-11-29 21:06:03 +0800

    fix: prevent switch case 2 tabs error

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
 	},
 	plugins: ['react'],
 	rules: {
-		indent: ['error', 'tab'],
+		indent: 'off',
 		'linebreak-style': ['error', 'unix'],
 		quotes: ['error', 'single'],
 		semi: ['error', 'never'],
