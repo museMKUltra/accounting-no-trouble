546546c92551ae97894dfe1d360ebe2e0742dc11
Roller Lo <jasonbh02@gmail.com>
2022-12-04 01:39:30 +0800

    refactor: abstract component Checkbox from CheckboxList

---
+++ src/components/Checkbox.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+function Checkbox({
+	checkbox,
+	checked,
+	disabled,
+	checkCheckbox,
+	uncheckCheckbox,
+	children,
+}) {
+	return (
+		<>
+			{' '}
+			<input
+				type="checkbox"
+				value={checkbox.key}
+				id={checkbox.key}
+				checked={checked}
+				disabled={disabled}
+				onChange={event => {
+					if (event.target.checked) {
+						checkCheckbox(checkbox.key)
+					} else {
+						uncheckCheckbox(checkbox.key)
+					}
+				}}
+			/>
+			<label
+				htmlFor={checkbox.key}
+				style={{ color: disabled ? 'lightgray' : 'black' }}
+			>
+				{checkbox.name}
+			</label>
+			{children}
+		</>
+	)
+}
+
+export default Checkbox
