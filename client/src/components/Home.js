009b515a667b2deea83b10e38d5e8ac5f07b1572
Roller Lo <jasonbh02@gmail.com>
2023-01-04 00:08:24 +0800

    feat: Home component uses Authorization

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,38 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
-import asana from 'asana'
+import { ClientContext } from '../contexts/ClientContext.js'
 
 function Home() {
 	const navigate = useNavigate()
-	const [gid, setGid] = useState('')
-	const [workspaces, setWorkspaces] = useState([])
-
-	useEffect(() => {
-		const accessToken = localStorage.getItem('access_token')
-		const refreshToken = localStorage.getItem('refresh_token')
-
-		if (!accessToken) {
-			navigate('/oauth/grant')
-		}
-
-		const fetchMe = async () => {
-			try {
-				const client = asana.Client.create().useAccessToken(accessToken)
-				const { gid = '', workspaces = [] } = await client.users.me()
-				setGid(gid)
-				setWorkspaces(workspaces)
-			} catch (e) {
-				alert(e)
-				localStorage.removeItem('access_token')
-				if (refreshToken) {
-					navigate('/oauth/refresh')
-				} else {
-					navigate('oauth/grant')
-				}
-			}
-		}
-		fetchMe()
-	}, [])
+	const { user } = useContext(ClientContext)
 
 	const logout = () => {
 		localStorage.removeItem('access_token')
@@ -60,18 +32,17 @@ function Home() {
 	}
 
 	return (
-		<div>
-			<div>gid: {gid}</div>
-			{workspaces.map(workspace => (
-				<div key={workspace.gid}> workspace: {workspace.gid}</div>
-			))}
+		<>
+			<div>{user.gid}</div>
+			<div>{user.name}</div>
+			<div>{user.email}</div>
 			<button type="button" onClick={logout}>
 				logout
 			</button>
 			<button type="button" onClick={revoke}>
 				revoke
 			</button>
-		</div>
+		</>
 	)
 }
 
