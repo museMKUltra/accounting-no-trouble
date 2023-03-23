5e59d375496a2d9fc3a59c98000a6282c6ad78b2
it.andy <andy.ai@kkday.com>
2023-03-23 17:45:40 +0800

    feat: 呈現工單backlog

--- client/src/components/Issue.js
+++ client/src/components/Issue.js
@@ -1,35 +1,63 @@
 import React, { useContext } from 'react'
 import BoardSection from './BoardSection.js'
 import { useSections } from '../hooks/asana/useSections.js'
+import { useDateline } from '../reducers/useDateline.js'
 import {
 	WORKSPACE_GID as workspaceGid,
 	PROJECT_GID_LIST as projectIdList,
 } from '../configs/constent.js'
 import { ClientContext } from '../contexts/ClientContext.js'
 import { GidContext } from '../contexts/GidContext.js'
+import { DatelineContext } from '../contexts/DatelineContext.js'
+import { ProportionContext } from '../contexts/ProportionContext.js'
+import {
+	useProportion,
+} from '../reducers/useProportion.js'
 
 function Issue() {
 	const projectGid = projectIdList.ISSUE
+	const issueSectionGid = '1201191083505009'
 	const { user } = useContext(ClientContext)
 	const { isFetching: isSectionsFetching, sections } = useSections({
 		projectGid,
 	})
-	const sectionList = sections.map(section =>
-		Object.assign(section, { key: section.gid })
+	const issueSection = sections.find(section =>
+		section.gid === issueSectionGid
 	)
+	const { dateline, proposeStartOn, proposeDueOn } = useDateline()
+	const {
+		accountingTasks,
+		appendAccountingTask,
+		deleteAccountingTask,
+	} = useProportion()
+
 	return (
 		<GidContext.Provider
 			value={{
 				workspaceGid,
 				projectGid,
 			}}>
-			{user.isFetching || isSectionsFetching ? (
-				<p>fetching...</p>
-			) : (
-				sectionList.map(section => (
-					<BoardSection key={section.key} section={section} />
-				))
-			)}
+			<DatelineContext.Provider
+				value={{
+					dateline,
+					proposeStartOn,
+					proposeDueOn,
+				}}
+			>
+				<ProportionContext.Provider
+					value={{
+						accountingTasks,
+						appendAccountingTask,
+						deleteAccountingTask,
+					}}
+				>
+					{user.isFetching || isSectionsFetching ? (
+						<p>fetching...</p>
+					) : (
+						<BoardSection key={issueSection.key} section={issueSection}/>
+					)}
+				</ProportionContext.Provider>
+			</DatelineContext.Provider>
 		</GidContext.Provider>
 		
 	)
