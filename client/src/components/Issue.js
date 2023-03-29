bae481f6a41a8417da2601e08b44002665b4426f
Roller Lo <roller.lo@kkday.com>
2023-03-29 14:35:48 +0800

    feat(Issue): fetch issue tasks by useTasksWithPayload

--- client/src/components/Issue.js
+++ client/src/components/Issue.js
@@ -1,66 +1,39 @@
-import React, { useContext } from 'react'
-import BoardSection from './BoardSection.js'
-import { useSections } from '../hooks/asana/useSections.js'
-import { useDateline } from '../reducers/useDateline.js'
+import React, {useEffect} from 'react'
+
 import {
 	WORKSPACE_GID as workspaceGid,
 	PROJECT_GID_LIST as projectIdList,
 } from '../configs/constent.js'
-import { ClientContext } from '../contexts/ClientContext.js'
-import { GidContext } from '../contexts/GidContext.js'
-import { DatelineContext } from '../contexts/DatelineContext.js'
-import { ProportionContext } from '../contexts/ProportionContext.js'
-import {
-	useProportion,
-} from '../reducers/useProportion.js'
+import {useTasksWithPayload} from '../hooks/asana/useTasksWithPayload'
+
+const issueProjectGid = projectIdList.ISSUE
+const issueSectionGid = '1201191083505009'
 
 function Issue() {
-	const projectGid = projectIdList.ISSUE
-	const issueSectionGid = '1201191083505009'
-	const { user } = useContext(ClientContext)
-	const { isFetching: isSectionsFetching, sections } = useSections({
-		projectGid,
-	})
-	const issueSection = sections.find(section =>
-		section.gid === issueSectionGid
-	)
-	const { dateline, proposeStartOn, proposeDueOn } = useDateline()
 	const {
-		accountingTasks,
-		appendAccountingTask,
-		deleteAccountingTask,
-	} = useProportion()
+		isFetching: isTasksFetching,
+		tasks,
+		fetchTasks,
+	} = useTasksWithPayload()
 
-	return (
-		<GidContext.Provider
-			value={{
-				workspaceGid,
-				projectGid,
-			}}>
-			<DatelineContext.Provider
-				value={{
-					dateline,
-					proposeStartOn,
-					proposeDueOn,
-				}}
-			>
-				<ProportionContext.Provider
-					value={{
-						accountingTasks,
-						appendAccountingTask,
-						deleteAccountingTask,
-					}}
-				>
-					{user.isFetching || isSectionsFetching ? (
-						<p>fetching...</p>
-					) : (
-						<BoardSection key={issueSection.key} section={issueSection}/>
-					)}
-				</ProportionContext.Provider>
-			</DatelineContext.Provider>
-		</GidContext.Provider>
-		
-	)
+	useEffect(() => {
+		async function fetchIssueTasks() {
+			await fetchTasks(workspaceGid, {
+				'projects.any': issueProjectGid,
+				'sections.any': issueSectionGid,
+			})
+		}
+
+		fetchIssueTasks()
+	}, [])
+
+	return <>
+		{isTasksFetching ? (
+			<p>fetching...</p>
+		) : (
+			<div>{JSON.stringify(tasks)}</div>
+		)}
+</>
 }
 
 export default Issue
