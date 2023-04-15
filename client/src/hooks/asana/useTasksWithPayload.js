30a3dcf333f2177da05a8a8d63f329944e276fc4
Roller Lo <jasonbh02@gmail.com>
2023-04-15 15:48:09 +0800

    docs(useTasksWithPayload): todo comment for refactoring

--- client/src/hooks/asana/useTasksWithPayload.js
+++ client/src/hooks/asana/useTasksWithPayload.js
@@ -1,6 +1,7 @@
 import { useCallback, useContext, useState } from 'react'
 import { ClientContext } from '../../contexts/ClientContext.js'
 
+// TODO: 整合至 /hooks/asanna/tasks 使用
 export function useTasksWithPayload() {
 	const [tasks, setTasks] = useState([])
 	const [isFetching, setIsFetching] = useState(false)
