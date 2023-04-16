bdfda3157c2eda65e81be4c94ce1aa0bb5dc7249
Roller Lo <jasonbh02@gmail.com>
2023-04-16 22:14:02 +0800

    feat(useTasks): refresh access token & fetch again
    - access token will expire in one hour
    - if api 401 then refresh & fetch again

--- client/src/hooks/asana/tasks/useTasks.js
+++ client/src/hooks/asana/tasks/useTasks.js
@@ -5,22 +5,33 @@ import { tasksForSectionGetter, subtaskForTaskCreator } from '../apis.js'
 export function useTasks() {
 	const [response, setResponse] = useState(null)
 	const [isFetching, setIsFetching] = useState(false)
-	const { client } = useContext(ClientContext)
+	const { client, accessTokenRefresher } = useContext(ClientContext)
 
 	class Fetcher {
 		constructor(callback) {
 			this.callback = callback
+			this.handleRefreshAccessToken = accessTokenRefresher(
+				async refresh => {
+					await refresh()
+					setTimeout(() => {
+						this.fetch()
+					})
+				},
+				error => {
+					console.error(error)
+				}
+			)
 		}
 
 		async fetch() {
 			try {
 				setIsFetching(true)
 
-				const response = await this.callback(...arguments)
+				const response = await this.callback()
 
 				setResponse(response)
 			} catch (e) {
-				console.error(e)
+				await this.handleRefreshAccessToken(e)
 			} finally {
 				setIsFetching(false)
 			}
@@ -30,9 +41,11 @@ export function useTasks() {
 	const getTasksForSection = useCallback(
 		async (sectionGid, payload = {}) => {
 			const getTasksForSection = tasksForSectionGetter(client)
-			const fetcher = new Fetcher(getTasksForSection)
+			const fetcher = new Fetcher(
+				getTasksForSection.bind(null, sectionGid, payload)
+			)
 
-			await fetcher.fetch(sectionGid, payload)
+			await fetcher.fetch()
 		},
 		[client]
 	)
@@ -40,9 +53,11 @@ export function useTasks() {
 	const createSubtaskForTask = useCallback(
 		async (taskGid, payload = {}) => {
 			const createSubtaskForTask = subtaskForTaskCreator(client)
-			const fetcher = new Fetcher(createSubtaskForTask)
+			const fetcher = new Fetcher(
+				createSubtaskForTask.bind(null, taskGid, payload)
+			)
 
-			await fetcher.fetch(taskGid, payload)
+			await fetcher.fetch()
 		},
 		[client]
 	)
